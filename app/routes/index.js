28c25203679f788eb080326123e92ece91af44be
Jiwon Kang <jiwonk42@gmail.com>
2017-04-01 19:32:27 -0700

    add new-rental component that save a rental

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('rental');
-  },
+    model() {
+        return this.store.findAll('rental');
+    },
 
-  actions: {
-    destroyRental(rental) {
-      rental.destroyRecord();
-      this.transitionTo('index');
+    actions: {
+        saveRental3(params) {
+            var newRental = this.store.createRecord('rental', params);
+            newRental.save();
+            this.transitionTo('index');
+        },
+
+        destroyRental(rental) {
+            rental.destroyRecord();
+            this.transitionTo('index');
+        }
     }
-  }
 });
